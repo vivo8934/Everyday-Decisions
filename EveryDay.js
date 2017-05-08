186192c4588585bc00235f648506922ce7539a65
vivo8934 <mfundo8934@gmail.com>
2017-05-08 15:18:47 +0200

    everyday

---
+++ EveryDay.js
@@ -0,0 +1,25 @@
+
+var button = document.querySelector('.Mybtn');
+
+
+button.addEventListener('click', function(){
+
+  var Height = document.querySelector('#height').value
+  var HeightUnits = document.querySelector('#heightunits').value
+  var Weight = document.querySelector('#weight').value
+  var WeightUnits = document.querySelector('#weightunits').value
+
+alert('vivo');
+  if (HeightUnits == "inches") Height /= 39.3700787;
+    if (WeightUnits == "lb") Weight /= 2.20462;
+   var BMI = Weight / Math.pow(Height, 2);
+
+   document.getElementById("output").innerHTML = Math.round(BMI * 100)/100;
+  if (BMI < 18.5) document.getElementById("comment").innerHTML = "Underweight";
+
+  if (BMI >= 18.5 && BMI <= 25) document.getElementById("comment").innerHTML = "Normal";
+  if (BMI >= 25 && BMI <= 30) document.getElementById("comment").innerHTML = "Obese";
+   if (BMI > 30) document.getElementById("comment").innerHTML = "Overweight";
+   document.getElementById("comment").value = output;
+
+})
